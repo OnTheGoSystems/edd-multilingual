8674f169b95d78f96b6e36c1f69fbbf8d64c6380
David Garcia Watkins <david.gw@onthegosystems.com>
2023-11-20 11:51:52 +0100

    Prepare for releasing 1.4.1

--- edd-multilingual.php
+++ edd-multilingual.php
@@ -3,7 +3,7 @@
 Plugin Name: Easy Digital Downloads Multilingual
 Plugin URI: https://wordpress.org/plugins/edd-multilingual/
 Description: A plugin to enable seamless integration between Easy Digital Downloads and WPML | <a href="https://wpml.org/documentation/related-projects/easy-digital-downloads-multilingual/?utm_source=plugin&utm_medium=gui&utm_campaign=eddml">Documentation</a>
-Version: 1.4.0
+Version: 1.4.1
 Author: OnTheGoSystems
 Author URI: http://www.onthegosystems.com/
 Text Domain: edd_multilingual
@@ -13,7 +13,7 @@ if ( defined( 'EDD_MULTILINGUAL_VERSION' ) ) {
 	return;
 }
 
-define( 'EDD_MULTILINGUAL_VERSION', '1.4.0' );
+define( 'EDD_MULTILINGUAL_VERSION', '1.4.1' );
 define( 'EDD_MULTILINGUAL_PATH', dirname( __FILE__ ) );
 
 require EDD_MULTILINGUAL_PATH . '/class-edd-multilingual.php';
